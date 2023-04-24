a772bd36c218a2968f1efdb939fc8c3f667c72af
Ainazik <akylbekovaai.1998@gmail.com>
2023-04-24 18:26:58 +0300

    first commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,21 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    private Timer timer;
+
+    public AnimalsCage(Timer timer){
+        this.timer = timer;}
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -16,4 +24,6 @@ public class AnimalsCage {
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
+
+    public Timer getTimer(){ return timer;}
 }
